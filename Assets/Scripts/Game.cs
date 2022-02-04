b509143527e9509c0ab062a2742ac799223ffe92
Aleksandr Bashkirov <alxx85@mail.ru>
2022-02-04 07:36:25 +0700

    ...

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    [SerializeField] private Bird _bird;
+    [SerializeField] private PipeGenerator _pipeGenerator;
+    [SerializeField] private StartScreen _startScreen;
+    [SerializeField] private GameOverScreen _gameOverScreen;
+
+    private void OnEnable()
+    {
+        _startScreen.PlayButtonClick += OnPlayButtonClick;
+        _gameOverScreen.RestartButtonClick += OnRestartButtonClick;
+        _bird.GameOver += OnGameOver;
+    }
+
+    private void OnDisable()
+    {
+        _startScreen.PlayButtonClick -= OnPlayButtonClick;
+        _gameOverScreen.RestartButtonClick -= OnRestartButtonClick;
+        _bird.GameOver -= OnGameOver;
+    }
+
+    private void Start()
+    {
+        Time.timeScale = 0;
+        _startScreen.Open();
+    }
+
+    public void OnGameOver()
+    {
+        Time.timeScale = 0;
+        _gameOverScreen.Open();
+    }
+
+    private void OnPlayButtonClick()
+    {
+        _startScreen.Closed();
+        StartGame();
+    }
+
+    private void OnRestartButtonClick()
+    {
+        _gameOverScreen.Closed();
+        _pipeGenerator.ResetPool();
+        StartGame();
+    }
+
+    private void StartGame()
+    {
+        Time.timeScale = 1;
+        _bird.ResetPlayer();
+    }
+}
